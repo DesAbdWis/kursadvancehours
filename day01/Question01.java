e56f5ae9a4f481d6ef07add65a763c62d81a0b90
serefahmet <serefahmet21@gmail.com>
2021-06-09 16:00:21 +0300

    v02

--- day01/Question01.java
+++ day01/Question01.java
@@ -10,6 +10,7 @@ public class Question01 {
 	 * Musteri karti varsa ve 10 urunden fazla alirsa %20, yoksa %15 indirim yapin 
 	 * Musteri karti yoksa ve 10 urunden fazla alirsa %15, 10 urunden az
 	 * alirsa %10 indirim yapin
+	 *
 	 */
 	public static void main(String[] args) {
 
